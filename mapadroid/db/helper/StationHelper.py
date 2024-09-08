ad2a7062bbf3a69cec3506e3b1c97cc756281604
JabLuszko <JabLuszko@users.noreply.github.com>
2024-09-08 16:25:43 +0200

    WIP: Stations

---
+++ mapadroid/db/helper/StationHelper.py
@@ -0,0 +1,21 @@
+from typing import Optional, List
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from mapadroid.db.model import Station
+from mapadroid.utils.DatetimeWrapper import DatetimeWrapper
+
+
+class StationHelper:
+    @staticmethod
+    async def get(session: AsyncSession, station_id: str) -> Optional[Station]:
+        stmt = select(Station).where(Station.station_id == station_id)
+        result = await session.execute(stmt)
+        return result.scalars().first()
+
+    @staticmethod
+    async def get_changed_since(session: AsyncSession, _timestamp: int) -> List[Station]:
+        stmt = select(Station).where(Station.last_updated > DatetimeWrapper.fromtimestamp(_timestamp))
+        result = await session.execute(stmt)
+        return result.scalars().all()
